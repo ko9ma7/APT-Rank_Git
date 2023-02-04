c9328c15193bf00a6f67f79e7f0810048694aab5
APT-Rank <aptranking@gmail.com>
2023-02-04 23:23:50 +0900

    매매/전세지수 업데이트

--- js/draw_chart.js
+++ js/draw_chart.js
@@ -488,17 +488,17 @@ function drawPriceRateChart(dateArray, salesArray, rentArray){
         label: "매매지수",
         data: salesData,        
         borderColor: "#ff3d38",
-        borderWidth: 2,
+        borderWidth: 1,
         backgroundColor: "#ff3d38",
-        pointRadius: 0
+        pointRadius: 1
       },
       {
         label: "전세지수",
         data: rentData,
         borderColor: "#5589c9",
-        borderWidth: 2,
+        borderWidth: 1,
         backgroundColor: "#5589c9",
-        pointRadius: 0
+        pointRadius: 1
       }
     ],
     },
@@ -519,7 +519,15 @@ function drawPriceRateChart(dateArray, salesArray, rentArray){
           ticks:{            
             stepSize: 50,            
           }
-        }
+        },
+        x: {
+          ticks: {                
+              maxRotation: 0,
+              font: {
+                size: 10
+              }
+          }
+        },
       },
       animation: {
         delay: 500, // change delay to suit your needs.
