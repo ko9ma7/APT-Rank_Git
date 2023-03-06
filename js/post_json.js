94e55be8c4d65b0fb85ab00efd691ac2644ed252
APT-Rank <aptranking@gmail.com>
2023-03-06 23:28:27 +0900

    fetch 가능 여부 확인

---
+++ js/post_json.js
@@ -0,0 +1,17 @@
+function post(aptName, log_date, short_name) {
+  const url = "https://www.aptrank.kr/log/202303.json"
+
+  fetch(url, {
+    method: "POST",
+    header:{
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      aptName: aptName,
+      region: short_name,
+      date: log_date,
+    }),
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+}
