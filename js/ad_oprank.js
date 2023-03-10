a04b54be7f36e2a6eaaf10c312b395fa939f1dc1
APT-Rank <aptranking@gmail.com>
2023-03-10 16:11:18 +0900

    광고 GTAG 업데이트

--- js/ad_oprank.js
+++ js/ad_oprank.js
@@ -89,7 +89,7 @@ function showPART(){
 
     //var ad_click_date = new Date();
     //gtag('event','파트너쉽선택', { 'event_category' : 'Click_Partnership', '파트너쉽선택' : shop_name + "\t" + ad_click_date + "\t" + ($('#sido option:selected').text() + " " + $('#gungu option:selected').text())});	
-    gtag('event','파트너쉽선택', { 'event_category' : 'Click_Partnership', 'event_label' : "오피스텔랭크," + today_str + "," + shop_name + ",클릭"});
+    gtag('event','파트너쉽선택', { 'event_category' : 'Click_Partnership', 'event_label' : "오피스텔랭크," + today_str + "," + shop_name + ",클릭,파트너쉽"});
     
     titleHtml += "<div id='part_title'>"        
         titleHtml += "<div id='part_title_sub'>"
