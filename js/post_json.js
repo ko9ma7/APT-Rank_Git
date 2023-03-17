be27992bbe3d5193060bec50d9ae873daf586943
APT-Rank <aptranking@gmail.com>
2023-03-17 17:03:04 +0900

    typicode 서버 실행

--- js/post_json.js
+++ js/post_json.js
@@ -1,11 +1,10 @@
 function post(aptName, log_date, short_name) {
-  const url = "https://github.com/APT-Rank/log/db"
+  const url = "https://my-json-server.typicode.com/APT-Rank/jsonlog/db"
 
   fetch(url, {    
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      "Access-Control-Allow-Origin": "*"
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       "aptName": aptName,
