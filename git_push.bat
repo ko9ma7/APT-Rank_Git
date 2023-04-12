9909ef29547844157b41718a493764e8a73e3e2f
APT-Rank <aptranking@gmail.com>
2023-04-12 21:42:46 +0900

    Realprice Auto Update

--- git_push.bat
+++ git_push.bat
@@ -3,6 +3,8 @@ D:
 cd APT-Rank_Git
 git add *
 git commit -m "Realprice Auto Update"
+git config --global credential.helper manager-core
+git config --global core.autocrlf true
 git push -u origin main
 
 pause
