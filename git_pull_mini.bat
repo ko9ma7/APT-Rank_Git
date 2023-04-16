98aa5c1df1c2a42887b12ac5ac6269dcaf399094
APT-Rank <aptranking@gmail.com>
2023-04-16 16:03:49 +0900

    Real-Price Auto Update

---
+++ git_pull_mini.bat
@@ -0,0 +1,3 @@
+cd\
+cd Users\anito\APT-Rank_Git
+git pull
---
+++ git_push_mini.bat
@@ -0,0 +1,9 @@
+cd\
+cd Users\anito\APT-Rank_Git
+git config --global core.autocrlf true
+git config --global credential.helper manager-core
+git add *
+git commit -m "Real-Price Auto Update"
+git push -u origin main
+
+pause
